c45f23ff9696aeb6de0862613b7bcfa724433f15
Paul Lourdu Xavier <34255718+plxavier@users.noreply.github.com>
2025-10-01 23:50:04 +0200

    added filter by email funciton

--- filter_users.py
+++ filter_users.py
@@ -19,9 +19,19 @@ def filter_users_by_age(age):
     for user in filtered_users:
         print(user)
 
+def filter_users_by_email(email):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users = [user for user in users if user["email"].lower() == email.lower()]
+
+    for user in filtered_users:
+        print(user)
+
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, 'name' and 'age' are supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (Currently, 'name', 'age' "
+                          "and 'email' are supported): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
@@ -29,5 +39,8 @@ if __name__ == "__main__":
     elif filter_option == "age":
         age_to_search = int(input("Enter an age (number 0 and above) to filter users: ").strip())
         filter_users_by_age(age_to_search)
+    elif filter_option == "email":
+        email_to_search = input("Enter the email (name@example.com) to filter users: ").strip()
+        filter_users_by_email(email_to_search)
     else:
-        print("Filtering by that option is not yet supported.")
+        print("Filtering by that option is not yet supported. Check your input.")
