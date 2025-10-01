71bdcb37a408ef6826412e0ef09e871ffd999c76
Paul Lourdu Xavier <34255718+plxavier@users.noreply.github.com>
2025-10-02 01:06:31 +0200

    added basic error handling

--- filter_users.py
+++ filter_users.py
@@ -2,45 +2,97 @@ import json
 
 
 def filter_users_by_name(name):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    """function for filtering out users by name"""
+    try:
+        with open("users.json", "r") as file:
+            users = json.load(file)
+    except FileNotFoundError:
+        print("No users.json found")
+        return
+    except Exception as error:
+        print(f"The error was {error}.")
+        return
 
-    filtered_users = [user for user in users if user["name"].lower() == name.lower()]
+    try:
+        filtered_users = [user for user in users if user["name"].lower() == name.lower()]
 
-    for user in filtered_users:
-        print(user)
+        if filtered_users:
+            for user in filtered_users:
+                print(user)
+        else:
+            print(f"No user with the name *{name}* found. Check your spelling and try again")
+
+    except Exception as error:
+        print(f"The error was {error}")
+
 
 def filter_users_by_age(age):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    """function for filtering out users by age"""
+    try:
+        with open("users.json", "r") as file:
+            users = json.load(file)
+    except FileNotFoundError:
+        print("No users.json found")
+        return
+    except Exception as error:
+        print(f"The error was {error}.")
+        return
 
-    filtered_users = [user for user in users if int(user["age"]) >= age]
+    try:
+        filtered_users = [user for user in users if int(user["age"]) >= age]
 
-    for user in filtered_users:
-        print(user)
+        if filtered_users:
+            for user in filtered_users:
+                print(user)
+        else:
+            print(f"No user with the given age cut-off {age} years and above found")
+    except Exception as error:
+        print(f"The error was {error}")
+
 
 def filter_users_by_email(email):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    """function for filtering out users by email"""
+    try:
+        with open("users.json", "r") as file:
+            users = json.load(file)
+    except FileNotFoundError:
+        print("No users.json found")
+        return
+    except Exception as error:
+        print(f"The error was {error}.")
+        return
 
-    filtered_users = [user for user in users if user["email"].lower() == email.lower()]
+    try:
+        filtered_users = [user for user in users if user["email"].lower() == email.lower()]
 
-    for user in filtered_users:
-        print(user)
+        if filtered_users:
+            for user in filtered_users:
+                print(user)
+        else:
+            print(f"No user with *{email}* found. Check your spelling and try again")
+
+    except Exception as error:
+        print(f"The error was {error}")
 
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, 'name', 'age' "
-                          "and 'email' are supported): ").strip().lower()
+    try:
+        filter_option = input("What would you like to filter by? (Currently, 'name', 'age' "
+                              "and 'email' are supported): ").strip().lower()
 
-    if filter_option == "name":
-        name_to_search = input("Enter a name to filter users: ").strip()
-        filter_users_by_name(name_to_search)
-    elif filter_option == "age":
-        age_to_search = int(input("Enter an age (number 0 and above) to filter users: ").strip())
-        filter_users_by_age(age_to_search)
-    elif filter_option == "email":
-        email_to_search = input("Enter the email (name@example.com) to filter users: ").strip()
-        filter_users_by_email(email_to_search)
-    else:
-        print("Filtering by that option is not yet supported. Check your input.")
+        if filter_option == "name":
+            name_to_search = input("Enter a name to filter users: ").strip()
+            filter_users_by_name(name_to_search)
+        elif filter_option == "age":
+            age_to_search = int(input("Enter the age (a number: 0 and above) to filter users: ").strip())
+            filter_users_by_age(age_to_search)
+        elif filter_option == "email":
+            email_to_search = input("Enter the email (name@example.com) to filter users: ").strip()
+            filter_users_by_email(email_to_search)
+        else:
+            print("Filtering by that option is not yet supported. Check your input.")
+
+    except ValueError as e:
+        print(f"The error was a value-error {e}. Try again.")
+    except Exception as e:
+        print(f"The error was {e}")
